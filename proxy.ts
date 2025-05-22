8961aaaf4c48374dbcb1485df39912246030de69
IAMNOTASPAMBOTYOUNOOB <thegreenk1dwhosceens@gmail.com>
2025-05-22 17:23:25 +0800

    fixed proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,21 +1,25 @@
-  import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 
-  serve(async (req) => {
-    const url = new URL(req.url);
-    const target = "https://api.ropro.io" + url.pathname + url.search;
-    const headers = new Headers(req.headers);
-    headers.set("origin", "https://ropro.io");
-    headers.set("referer", "https://ropro.io/");
-    headers.set("user-agent", "RoPro");
+serve(async (req) => {
+  const url = new URL(req.url);
+  const targetUrl = `https://api.ropro.io${url.pathname}${url.search}`;
 
-    const response = await fetch(target, {
+  const headers = new Headers(req.headers);
+  headers.set("origin", "https://ropro.io");
+  headers.set("referer", "https://ropro.io/");
+  headers.set("user-agent", "RoPro");
+
+  try {
+    const res = await fetch(targetUrl, {
       method: req.method,
       headers,
       body: req.body,
     });
-
-    return new Response(response.body, {
-      status: response.status,
-      headers: response.headers,
+    return new Response(res.body, {
+      status: res.status,
+      headers: res.headers,
     });
-  });
+  } catch {
+    return new Response("Error contacting RoPro API", { status: 502 });
+  }
+});
